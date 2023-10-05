e6378aa1189a4e2445c2fca92e0e2c342a96926d
abdulshaikh55 <116955072+abdulshaikh55@users.noreply.github.com>
2023-10-05 15:54:00 +0530

    Project Alpha

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Task Manager CLI with a TUI (Text-based User Interface)
 ## Description
-This command line application made with Rust allows users to manage their tasks with a text-based user interface (TUI).
+This command line application to be made with Rust allows users to manage their tasks with a text-based user interface (TUI).
+This project will incorporate several Rust basics, including structs, enums, modules,  file I/O, error handling, and user input processing.
+
+## Features
+1. Task CRUD operations: Users can create, read, update, and delete tasks. Each task should have a title, description, priority level and due date. 
+2. 
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,60 @@
-extern crate tui;
-use tui::{Terminal, terminal};
-use tui::backend::CrosstermBackend;
-use tui::widgets::{Widget, Block, Borders};
-use tui::layout::{Layout, Constraint, Direction};
-use std::fs;
-enum Priority {
-    Low, Medium, High
-}
-enum Status {
-    NotStarted, Ongoing, Completed
-}
-struct Task {
-    title: String,
-    description: String,
-    priority: Priority,
-    due_date: String,
-    status: Status
+use std::io::Write; // used by .flush() in add_task funtion
+
+// Abdul, let's start with creating a function that reads the task from the user.
+#[allow(dead_code)]
+fn add_task() -> String{
+    let mut task:String = String::new();
+    print!("➕Add task: ");
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut task).expect("Failed to read input");
+    let task: String = task.trim().parse().ok().expect("Enter a valid input");
+    task
 }
 
-pub mod TaskHandling{
-    pub fn add_task() {
-
+// Now, let's create a funtion that displays all the tasks
+#[allow(dead_code)]
+fn display_all(tasks: Vec<String>) {
+    for (idx, task) in tasks.iter().enumerate() {
+        println!("{} -> {}", idx, *task); 
     }
-
-    pub fn update_task() {
-
-    }
-
-    pub fn delete_task() {
-
-    }
-
 }
 
 fn main() {
-    // Create a TUI backend for the Terminal
-    let backend = CrosstermBackend::new(io::stdout());
+    // a vector type DS that will hold all the tasks.
+    let mut tasks: Vec<String> = Vec::new();
+    
+    println!("Hello User! How can I help you?");
+    loop {
+        print!("******************\n1. Add Task\n2. Delete Task\n3. Display list\n4. Exit\nChoose an option: ");
+        std::io::stdout().flush().unwrap();
+        let mut choice: String = String::new();
+        std::io::stdin().read_line(&mut choice).expect("Failed to read input");
+        let choice: u8 = choice.trim().parse().expect("Enter a valid input");
+        if !(1..=4).contains(&choice) {
+            println!("❌Invalid input. Try again");
+            continue; // Jump back to the start of the loop
+        }
+        match choice {
+            1 => { // adds new task to the list
+                let new_task: String = add_task();
+                tasks.push(new_task);
+                println!("✅Task list updated.");
+            },
 
-    // Create a TUI terminal using the backend
-    let mut terminal = terminal::new(backend).unwrap();
+            2 => { // to delete a specific task or all the tasks
+
+            },
+
+            3 => { // To display all the tasks
+                display_all(tasks.clone())
+            },
+
+            4 => { // ro exit the program
+                break;
+            },
+
+            _ => (),
+        }
+    }
+    
 }
