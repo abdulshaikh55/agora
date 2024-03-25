4cefc0c0fdc70e206b0574e6899e0ae7626eea1a
abdulshaikh55 <116955072+abdulshaikh55@users.noreply.github.com>
2024-03-25 07:29:33 +0530

    project Lambda (Zeta)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 mod app;
-mod controls;
 mod task_management;
 mod ui;
 
 use app::{App, CurrentScreen, CurrentlyEditing};
-use controls::StatefulList;
-use task_management::{Priority, Status, Task, TaskManager};
+use task_management::{StatefulList, Task, TaskManager};
 
 use crossterm::event::{Event, KeyCode};
 use crossterm::terminal::{
@@ -14,66 +12,36 @@ use crossterm::terminal::{
 use crossterm::{event, execute};
 use ratatui::backend::CrosstermBackend;
 use ratatui::Terminal;
-//use std::fs::File;
+use std::fs::File;
 use std::io::stderr;
-// use std::io::{Read, Write};
+use std::io::{Read, Write};
 
 fn main() -> std::io::Result<()> {
+    // 🚀 Prepare to enter alternate screen
     enable_raw_mode()?;
     execute!(stderr(), EnterAlternateScreen)?;
 
     let mut stderr = stderr();
     execute!(stderr, EnterAlternateScreen)?;
 
+    // create the terminal on backend
     let backend = CrosstermBackend::new(stderr);
     let mut terminal = Terminal::new(backend)?;
 
-    let tasks: Vec<Task> = vec![
-        Task {
-            task: "Eat".to_string(),
-            priority: Priority::Important,
-            status: Status::NotStarted,
-        },
-        Task {
-            task: "Code".to_string(),
-            priority: Priority::Important,
-            status: Status::NotStarted,
-        },
-        Task {
-            task: "Sleep".to_string(),
-            priority: Priority::Important,
-            status: Status::NotStarted,
-        },
-        Task {
-            task: "Repeat".to_string(),
-            priority: Priority::Important,
-            status: Status::NotStarted,
-        },
-    ];
-    // vec![
-    //     "Eat".to_string(),
-    //     "Code".to_string(),
-    //     "Sleep".to_string(),
-    //     "Repeat".to_string(),
-    // ];
+    // read data from .json file.
+    let mut file = File::open("tasks.json")?;
+    let mut file_content = String::new();
+    let mut tasks: Vec<Task> = Vec::new();
+    if file.read_to_string(&mut file_content)? != 0 {
+        tasks = serde_json::from_str(&file_content)?;
+    }
 
     let mut app = App::new();
-    let mut task_manager = TaskManager::new();
-    task_manager.insert_tasks(tasks);
+    let mut task_manager = TaskManager::new(tasks);
     let mut task_with_state = StatefulList::new(&task_manager.tasks);
 
-    // // write data into tasks.json
-    // let data = serde_json::to_string(&task_manager)?;
-    // let mut file = File::create("tasks.json")?;
-    // file.write_all(data.as_bytes())?;
-
-    // let mut file = File::open("tasks.json")?;
-    // let mut file_content = String::new();
-    // file.read_to_string(&mut file_content)?;
-    //let tasks: Vec<String> = serde_json::from_str(&file_content)?;
-
     loop {
-        terminal.draw(|f| ui::ui(f, &mut task_with_state, &app, &task_manager))?;
+        terminal.draw(|f| ui::ui(f, &app, &task_manager, &mut task_with_state))?;
 
         if event::poll(std::time::Duration::from_millis(16))? {
             if let Event::Key(key) = event::read()? {
@@ -83,8 +51,7 @@ fn main() -> std::io::Result<()> {
                 match app.current_screen {
                     CurrentScreen::Main => match key.code {
                         // Go to exit screen when [Esc] is pressed
-                        KeyCode::Esc => break, // app.change_screen(CurrentScreen::Exiting),
-                        // Go to new screen when [=] is pressed
+                        KeyCode::Esc => app.change_screen(CurrentScreen::Exiting),
                         KeyCode::Enter => app.change_screen(CurrentScreen::New),
                         // Task list navigation section
                         KeyCode::Right => app.change_screen(CurrentScreen::Task),
@@ -104,6 +71,13 @@ fn main() -> std::io::Result<()> {
                             }
                             KeyCode::Up | KeyCode::Down => app.toggle_task_priority(),
                             KeyCode::Right | KeyCode::Left => app.toggle_priority_status(),
+                            KeyCode::Enter => {
+                                if !task_manager.input_task_string.is_empty() {
+                                    task_manager.save_instance_task();
+                                    task_with_state = StatefulList::new(&task_manager.tasks);
+                                }
+                                app.change_screen(CurrentScreen::Main)
+                            }
                             _ => (),
                         },
                         CurrentlyEditing::Priority => match key.code {
@@ -111,6 +85,13 @@ fn main() -> std::io::Result<()> {
                             KeyCode::Up | KeyCode::Down => app.toggle_task_priority(),
                             KeyCode::Right | KeyCode::Left => app.toggle_priority_status(),
                             KeyCode::Tab => task_manager.switch_priority_value(),
+                            KeyCode::Enter => {
+                                if !task_manager.input_task_string.is_empty() {
+                                    task_manager.save_instance_task();
+                                    task_with_state = StatefulList::new(&task_manager.tasks);
+                                }
+                                app.change_screen(CurrentScreen::Main)
+                            }
                             _ => (),
                         },
                         CurrentlyEditing::Status => match key.code {
@@ -118,6 +99,13 @@ fn main() -> std::io::Result<()> {
                             KeyCode::Up | KeyCode::Down => app.toggle_task_priority(),
                             KeyCode::Right | KeyCode::Left => app.toggle_priority_status(),
                             KeyCode::Tab => task_manager.switch_status_value(),
+                            KeyCode::Enter => {
+                                if !task_manager.input_task_string.is_empty() {
+                                    task_manager.save_instance_task();
+                                    task_with_state = StatefulList::new(&task_manager.tasks);
+                                }
+                                app.change_screen(CurrentScreen::Main)
+                            }
                             _ => (),
                         },
                     },
@@ -128,19 +116,17 @@ fn main() -> std::io::Result<()> {
                     },
                     CurrentScreen::Editing => match key.code {
                         KeyCode::Left => app.change_screen(CurrentScreen::Task),
-                        // KeyCode::Char(c) => task_manager.tasks,
                         KeyCode::Down => (),
                         _ => (), // app.change_screen(CurrentScreen::Editing(Some(edit)));
                     },
-                    CurrentScreen::Exiting => (),
-                    // match key.code {
-                    //     KeyCode::Char('y') => break,
-                    //     KeyCode::Char('n') => {
-                    //         app.change_screen(CurrentScreen::Main);
-                    //         continue;
-                    //     }
-                    //     _ => (),
-                    // },
+                    CurrentScreen::Exiting => match key.code {
+                        KeyCode::Char('y') => break,
+                        KeyCode::Char('n') => {
+                            app.change_screen(CurrentScreen::Main);
+                            continue;
+                        }
+                        _ => (),
+                    },
                 }
             }
         }
@@ -149,7 +135,10 @@ fn main() -> std::io::Result<()> {
     disable_raw_mode()?;
     execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
 
-    // todo!("write data");
+    // write data into tasks.json
+    let data = serde_json::to_string(&task_manager.tasks)?;
+    let mut file = File::create("tasks.json")?;
+    file.write_all(data.as_bytes())?;
 
     Ok(())
 }
